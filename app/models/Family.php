a3fe9fa64c1fa1fecfe5d2bfbba6e114d61506bd
koykhaled <koykhaled@gmail.com>
2023-05-25 14:36:25 +0300

    set get

--- app/models/Family.php
+++ app/models/Family.php
@@ -13,6 +13,58 @@ class Family extends Model
         $location_id,
         $individuals_number;
 
+    public function setFname($fname)
+    {
+        $this->fname = $fname;
+    }
+    public function setMname($mname)
+    {
+        $this->mname = $mname;
+    }
+    public function setLname($lname)
+    {
+        $this->lname = $lname;
+    }
+
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+    public function setLocid($location_id)
+    {
+        $this->location_id = $location_id;
+    }
+    public function setPersonNumber($number)
+    {
+        $this->individuals_number = $number;
+    }
+
+    public function getFname()
+    {
+        return $this->fname;
+    }
+    public function getMname()
+    {
+        return $this->mname;
+    }
+    public function getLname()
+    {
+        return $this->lname;
+    }
+
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+    public function getPersonNumber()
+    {
+        return $this->individuals_number;
+    }
+    public function getLocId()
+    {
+        return $this->location_id;
+    }
+
     public function getAllFamilies()
     {
     }
